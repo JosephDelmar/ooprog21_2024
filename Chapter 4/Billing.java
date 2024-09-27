0e36a528ed2aeb8f4d7f3522a2f3d79192d133c1
JosephDelmar <Josephhdelmar@gmail.com>
2024-09-27 18:36:48 +0800

    Create Billing.java

---
+++ Chapter 4/Billing.java
@@ -0,0 +1,29 @@
+public class Billing {
+
+public void computeBill (double price) {
+
+double total price (price 8.08);
+
+System.out.printf("The total price for the bill is: $%.15fkn", total);
+
+public void computeßill (double price, int quantity) {
+
+double total (price quantity) (price quantity 8.08);
+
+System.out.printf("The total price for the bill is: 58.15fke", total);
+
+public void computeBill(double price, int quantity, int couponDiscount) {
+
+double subtotal (price quantity); double discount (couponDiscount/180.8) subtotal;
+
+double total (subtotal discount) ((subtotal discount) 0.08); System.out.printf("The total price for the bill is: .15%n", total);
+
+public static void main(String[] args) {
+
+Billing billing Billing()
+
+billing.computeBill(23.2);
+
+billing.computebil1(23.2, 3);
+
+billing.computebill(23.2, 3, 10);
