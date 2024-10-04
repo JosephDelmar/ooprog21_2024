fea36a810b1e9594e3810c8d9c69827efa03c8d7
JosephDelmar <Josephhdelmar@gmail.com>
2024-10-04 21:07:15 +0800

    Create TestBloodData.java

---
+++ Chapter 4/TestBloodData.java
@@ -0,0 +1,13 @@
+public class TestBloodData {
+
+public static void main(String[] args) { BloodData patient = new BloodData();
+
+ data patient.defaultPatient();
+
+ patient.setBloodType("B"); patient.setRhFactor("+");
+
+ patient.displayBloodInfo();
+
+}
+
+}
